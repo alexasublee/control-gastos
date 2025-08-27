7d2b25a03626d1b6282d51b58cbe0aa4db7e671f
Jonycakes03 <suarezjohnatan03@gmail.com>
2025-08-27 07:53:17 -0600

    Add: Comienzo app contrl gastos

--- App.js
+++ App.js
@@ -2,10 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const {nuevoGasto, setNuevoGasto} = useState('');
+  const {costoGasto, setCostoGasto} = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>App de Control de Gastos</Text>
+      <View style={styles.box}></View>
+      <TextInput placeholder="Ingresa titulo del gasto" 
+      value={nuevoGasto} 
+      onchangeText={setNuevoGasto}
+      style={styles.input}/>
+      <TextInput placeholder="Ingresa costo del gasto"
+      value={costoGasto}
+      onchangeText={setCostoGasto}
+      style={styles.input} 
+      keyboardType="numeric"/>
+    
     </View>
   );
 }
@@ -17,4 +30,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  box:{
+    width:100,
+    height:100,
+    backgroundColor:'lightblue',
+    borderWidth:2,
+  }, 
+  title:{
+    fontSize:20,
+    fontWeight:'bold',
+    color:'grey',
+  }
 });
